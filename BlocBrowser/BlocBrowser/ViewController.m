cf81aa67ef07bd03d362545baf9f086b2eea250e
Matt Krueger <mpkrueger@gmail.com>
2015-06-19 15:36:31 -0700

    Enable string entered into URL bar to have spaces and initiate a search

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -36,7 +36,7 @@
     self.textField.returnKeyType = UIReturnKeyDone;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString(@"Website URL or search", @"Placeholder text for web browser URL field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
     self.textField.delegate = self;
     
@@ -112,6 +112,14 @@
     if (!URL.scheme) {
         // The user didn't type http: or https:
         URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+    }
+    
+    if ([URLString rangeOfString:@" "].location != NSNotFound) {
+        NSArray *separateWords = [URLString componentsSeparatedByString:@" "];
+        NSString *newString = [separateWords componentsJoinedByString:@"+"];
+        NSLog(@"%@", newString);
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.google.com/search?q=%@", newString]];
+        NSLog(@"%@", URL);
     }
     
     if (URL) {
